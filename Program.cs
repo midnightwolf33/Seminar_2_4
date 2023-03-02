30612caecaa112cc65a4379717562bfb9349a6e7
midnight33 <gra33@bk.ru>
2023-03-02 19:14:19 +0300

    Задача 4 - готова

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача необязательная FIZZ BUZZ
+Enumerable.Range(1, 100).ToList().ForEach(_ =>
+            {
+                Console.WriteLine(_ % 3 != 0 && _ % 5 != 0
+                    ? _.ToString()
+                    : (_ % 3 == 0 ? "Fizz" : String.Empty)
+                    + (_ % 5 == 0 ? "Buzz" : String.Empty));
+            });
